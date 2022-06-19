30ded7555f5247992e69bfd5f3a43e729feff72f
EricData77 <ericdataguy77@gmail.com>
2022-06-20 08:32:13 +1000

    BackspaceCompare

--- FAANG Interviews/4StringBackSpace.js
+++ FAANG Interviews/4StringBackSpace.js
@@ -1,7 +1,9 @@
-S = "ab#c";
-T = "ad#c";
+const S = "abc#d";
+const T = "abzz##d";
 
-const builtString = function(strings) {
+// SOLUTION 1
+
+function builtString(strings) {
     let returnString = [];
     for (let i = 0; i < strings.length; i++) {
         if(strings[i] !== '#') {
@@ -15,14 +17,14 @@ const builtString = function(strings) {
 }
 
 const compareStrings = function(S, T) {
-    let s = builtString(S);
-    let t = builtString(T);
-    if (s.length !== t.length) {
+    let finalS = builtString(S);
+    let finalT = builtString(T);
+    if (finalS.length !== finalT.length) {
         return false;
     }
     else {
-        for (let i=0; i < strings.length; i++) {
-            if(s[i] !== t[i]){
+        for (let i=0; i < finalS.length; i++) {
+            if(finalS[i] !== finalT[i]){
                 return false;
             }
         }
@@ -30,5 +32,48 @@ const compareStrings = function(S, T) {
     return true;
 }
 
+// SOLUTION 2
+function backSpaceCompare(S, T){
+    let p1 = S.length - 1, p2 = T.length - 1;
+    while(p1 >= 0 || p2 >= 0) {
+        console.log({"p1":p1, "p2": p2});
+        if(S[p1] === "#" || T[p2] === "#") {
+            if(S[p1] === "#") {
+                let backCount = 2;
+                while (backCount > 0) {
+                    p1 --;
+                    backCount --;
+                    console.log({"BackCount 1": backCount});
+                    console.log({"P1 bc": p1});
+                    if(S[p1] === "#") {
+                        backCount += 2;
+                    }
+                }
+            }
+            if(T[p2] === "#") {
+                let backCount = 2;
+                while (backCount > 0) {
+                    p2 --;
+                    backCount --;
+                    console.log({"BackCount 2": backCount});
+                    console.log({"P2 bc": p2});
+                    if(T[p2] === "#") {
+                        backCount += 2;
+                    }
+                }
+            }
 
-console.log(compareStrings(S, T));
+        } else {
+            if (S[p1] !== T[p2]) {
+                return false;
+            } else {
+                p1 --;
+                p2 --;
+            }
+        }
+    }
+    return true;
+}
+
+
+console.log(backSpaceCompare(S, T));
