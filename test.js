e9f4e4ebcf70b2c3557b925ea773e32fa5520998
EricData77 <ericdataguy77@gmail.com>
2022-08-10 21:13:18 +1000

    QuickSelectionSort

--- test.js
+++ test.js
@@ -1,27 +1,40 @@
-const string = "{()[]}"
+const array = [5,3,1,6,4,2];
+const kToFind = 4;
 
-const parens = {
-    '(': ')',
-    '{' : '}',
-    '[': ']'
-}
-
-var isValid = function(s) {
-  if(s.length === 0) return true;
-  
-  const stack = [];
-  
-  for(let i = 0; i < s.length; i++)  {
-    if(parens[s[i]]) {
-      stack.push(s[i]);
-    } else {
-      const leftBracket = stack.pop();
-      const correctBracket = parens[leftBracket];
-      if(s[i] !== correctBracket) return false
-    }
-  }
-  
-  return stack.length === 0;
+const swap = function (array, i, j) {
+  const temp = array[i];
+  array[i] = array[j];
+  array[j] = temp;
 };
 
-console.log(isValid(string));
+const getPartition = function (nums, left, right) {
+  let i = left;
+
+  for (let j = left; j <= right; j++) {
+    if (nums[j] <= nums[right]) {
+      swap(nums, i, j);
+      i++;
+    }
+  }
+  return i - 1;
+};
+
+const quickSelect = function (nums, left, right, indexToFind) {
+  const partitionIndex = getPartition(nums, left, right);
+
+  if (partitionIndex === indexToFind) {
+    return nums[partitionIndex];
+  } else if (indexToFind < partitionIndex) {
+    return quickSelect(nums, left, partitionIndex - 1, indexToFind);
+  } else {
+    return quickSelect(nums, partitionIndex + 1, right, indexToFind);
+  }
+};
+
+var findKthLargest = function (nums, k) {
+  const indexToFind = nums.length - k;
+
+  return quickSelect(nums, 0, nums.length - 1, indexToFind);
+};
+
+console.log(findKthLargest(array, kToFind))
